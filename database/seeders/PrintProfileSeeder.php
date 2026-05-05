c2b38d829144a3eb5fb0ec4cc84d4d52a6d26eb1
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2026-05-05 09:59:50 +0700

    chore: remove A4 sticker profile and make Tom & Jerry 103 default

--- database/seeders/PrintProfileSeeder.php
+++ database/seeders/PrintProfileSeeder.php
@@ -10,23 +10,6 @@ class PrintProfileSeeder extends Seeder
     public function run(): void
     {
         PrintProfile::updateOrCreate(
-            ['name' => 'Default A4 Stickers (70x37mm)'],
-            [
-                'page_width_mm' => 210,
-                'page_height_mm' => 297,
-                'grid_columns' => 3,
-                'grid_rows' => 8,
-                'offset_x_mm' => 0,
-                'offset_y_mm' => 0,
-                'slot_width_mm' => 70,
-                'slot_height_mm' => 37,
-                'gap_x_mm' => 0,
-                'gap_y_mm' => 0,
-                'is_default' => true,
-            ]
-        );
-
-        PrintProfile::updateOrCreate(
             ['name' => 'Tom & Jerry 103 (63x31mm)'],
             [
                 'page_width_mm' => 210,
@@ -39,7 +22,7 @@ class PrintProfileSeeder extends Seeder
                 'slot_height_mm' => 31,
                 'gap_x_mm' => 4,
                 'gap_y_mm' => 7,
-                'is_default' => false,
+                'is_default' => true,
             ]
         );
     }
